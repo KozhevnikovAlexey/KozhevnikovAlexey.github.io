c856b191754d18039890baa75ec8521bf09de99c
Алексей Кожевников <aliosha9997@hotmail.com>
2018-06-10 21:08:42 +0300

    2018-06-10
    shop =slider

--- uplab2.0/js/custom.js
+++ uplab2.0/js/custom.js
@@ -11,6 +11,42 @@ $(function(){
 
 	$(window).bind('load resize', hf);
 
+	$('.baner-slider').slick({
+		arrows:false,
+		dots: true, 
+	}) 
+	$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+	});
+	
+$('.lazy').slick({
+  lazyLoad: 'ondemand',
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+	
 });
 
    
